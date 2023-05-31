bb4fbd7a8186e44a5e4baf0aa4888c5eaa99029f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-31 17:39:45 +0100

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="app">
-    <h1>{{ name }}</h1>
+    <h1>{{ name }} - {{  age }}</h1>
     <button @click="changeName('Zelda')">change name</button>
+    <button @click="changeAge(30)">change age</button>
   </div>
 </template>
 
 <script lang="ts">
+
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -15,12 +17,20 @@ export default defineComponent({
   },
   data() {
     return {
-      name: 'Hyrule Jobs'
+      name: 'Hyrule Jobs',
+      age: 25 as number | string
     }
   },
   methods: {
     changeName(name: string) {
       this.name = name
+
+      return name
+    },
+    changeAge(age: string | number) {
+      this.age = age
+      
+      return age
     }
   }
 });
