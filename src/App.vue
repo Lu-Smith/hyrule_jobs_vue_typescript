be2cf82e13e1f31788516efcfd8d6e9ee0a66a87
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-31 12:44:40 +0100

    Initial folders

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
