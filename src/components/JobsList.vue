6ce2fefba609cd46040a78e1617056d655a7d7f3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-02 00:12:53 +0100

    Updated JobsList.vue logic

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -3,7 +3,7 @@
   <p>Ordered by {{ order }}</p>
   <div class="job-list">
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary"> 
             <p>
@@ -17,7 +17,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 import Job from '@/types/Job'
 import OrderTerm from '@/types/OrderTerm';
 
@@ -32,6 +32,14 @@ export default  defineComponent ({
         required: true,
         type: String as PropType<OrderTerm>
     }
+ },
+ setup(props) {
+    const orderedJobs = computed(() => {
+        return [...props.jobs].sort((a: Job, b: Job) => {
+            return a[props.order] > b[props.order] ? 1 : -1
+        })
+    })
+    return { orderedJobs }
  }
 })
 </script>
