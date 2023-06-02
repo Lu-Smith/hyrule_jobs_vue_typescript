99d2260d89f7fe9606f6fed2c0cf31ce5d57829a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-02 22:45:23 +0100

    Updated MainFooter.vue

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,12 +1,8 @@
 <template>
-  Coded by Luna Smith
+    <footer>Coded by <a href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a>, open-sourced on 
+    <a href="https://github.com/Lu-Smith/hyrule_jobs_vue_typescript" target="_blank">GitHub</a>.</footer>
 </template>
 
-<script>
-export default {
-
-}
-</script>
 
 <style>
 
