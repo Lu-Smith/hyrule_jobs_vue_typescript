d5a637b1333067c2bc2807a962999b30a4b402d9
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-02 22:49:13 +0100

    Updated MainFooter,vue

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -3,7 +3,23 @@
     <a href="https://github.com/Lu-Smith/hyrule_jobs_vue_typescript" target="_blank">GitHub</a>.</footer>
 </template>
 
-
 <style>
-
+ footer {
+      position: fixed;
+      bottom: 0;
+      right: 0;
+      left: 0;
+      text-align: center;
+      background-color: #f2f2f2;
+      padding: 20px;
+  
+  }
+  
+  footer a {
+    color: #c93c11;
+    text-decoration: none;
+    position: relative;
+    transition: color 0.3s;
+  }
+  
 </style>
